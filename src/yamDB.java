d883f845d2c5f29044fe85369fc96643d5153c39
Lola Howell <lola2010@uw.edu>
2016-08-03 10:14:01 -0700

    Added Dropdown, Text Boxes, Radio Buttons

--- src/yamDB.java
+++ src/yamDB.java
@@ -8,18 +8,14 @@ import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.DefaultListModel;
-import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
-import javax.swing.table.DefaultTableModel;
-
-
-import java.awt.FlowLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -29,24 +25,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.List;
-import java.awt.Color;
-import java.awt.Dimension;
 
 public class yamDB {
 
 	private JFrame frame;
-	 private JTextField jtxtFieldName;
-	 private JTextField jtxtFieldYear;
-     private JButton btnSearch;
-     private DefaultListModel<String> model;
+	private JTextField jtxtFieldName;
+	private JTextField jtxtFieldActor;
+	private JTextField jtxtFieldYearFrom;
+	private JTextField jtxtFieldYearTo;
+	private JButton btnSearch;
+	private DefaultListModel<String> model;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-        Splash mySplash = new Splash(2000);
-        mySplash.showSplash();
+		Splash mySplash = new Splash(2000);
+		mySplash.showSplash();
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -83,42 +78,101 @@ public class yamDB {
 		ImageIcon icon = createImageIcon("logo.png","");
 		lbIcon.setIcon(icon);
 
-		//search by name
+		
+		JPanel innerPan1 = new JPanel();
+		innerPan1.setLayout(new BoxLayout(innerPan1, BoxLayout.X_AXIS));
+		JRadioButton searchButton = new JRadioButton(" SEARCH");
+		searchButton.setSelected(true);
+		JRadioButton rouleteButton = new JRadioButton(" ROULETTE");
+		JRadioButton predictorButton = new JRadioButton(" PREDICTOR");
+		innerPan1.add(searchButton);
+		innerPan1.add(rouleteButton);
+		innerPan1.add(predictorButton);
+		/*searchButton.addActionListener(this);
+	    rouleteButton.addActionListener(this);
+	    predictorButton.addActionListener(this);
+		public void actionPerformed(ActionEvent e) {
+		    ;
+		}*/
+		
+		
+		//search by movie title
 		JPanel innerPan = new JPanel();
 		innerPan.setLayout(new BoxLayout(innerPan, BoxLayout.X_AXIS));
-		JLabel lb1 = new JLabel("Search by Name :  ");
-	    jtxtFieldName = new JTextField();
+		JLabel lb1 = new JLabel("Movie Title  ");
+		jtxtFieldName = new JTextField();
 		innerPan.add(lb1);
 		innerPan.add(jtxtFieldName);
 
-		//search by year
+		//year from
 		JPanel innerPan2 = new JPanel();
 		innerPan2.setLayout(new BoxLayout(innerPan2, BoxLayout.X_AXIS));		
-		JLabel lb2 = new JLabel("Search by Year :  ");
-		/*JComboBox years = new JComboBox();
-		ArrayList<Integer> yesrList = new ArrayList();
-		for(int i = 1878; i < 2017; i++){
-			yesrList.add(i);
-			years.addItem(i);
-		}*/
-		jtxtFieldYear = new JTextField();
-		
-		//years.addItem(yesrList);
-		
-		innerPan2.add(lb2);
-		//innerPan2.add(years);
-		innerPan2.add(jtxtFieldYear);
-		//search button
+		JLabel yearfromlb = new JLabel("Year From  ");
+		jtxtFieldYearFrom = new JTextField();
+		innerPan2.add(yearfromlb);
+		innerPan2.add(jtxtFieldYearFrom);
+
+		//year to
+		JLabel yeartolb = new JLabel("  To  ");
+		jtxtFieldYearTo = new JTextField();
+		innerPan2.add(yeartolb);
+		innerPan2.add(jtxtFieldYearTo);
+
+		//actor
 		JPanel innerPan3 = new JPanel();
-		innerPan3.setLayout(new BorderLayout());
+		innerPan3.setLayout(new BoxLayout(innerPan3, BoxLayout.X_AXIS));	
+		JLabel actorlb = new JLabel("Actor's Name  ");
+		jtxtFieldActor = new JTextField();
+		innerPan3.add(actorlb);
+		innerPan3.add(jtxtFieldActor);
+
+		//rating From
+		JPanel innerPan4 = new JPanel();
+		innerPan4.setLayout(new BoxLayout(innerPan4, BoxLayout.X_AXIS));	
+		JLabel ratingsfromlb = new JLabel("Rating From  ");
+		JComboBox ratingsfrom = new JComboBox();
+		ArrayList<Integer> ratingsfromList = new ArrayList();
+		ratingsfrom.addItem(" ");
+		for(int i = 1; i < 11; i++){
+			ratingsfromList.add(i);
+			ratingsfrom.addItem(i);
+		}
+		innerPan4.add(ratingsfromlb);
+		innerPan4.add(ratingsfrom);
+
+		//rating To
+		JLabel ratingstolb = new JLabel("  To  ");
+		JComboBox ratingsto = new JComboBox();
+		ArrayList<Integer> ratingstoList = new ArrayList();
+		ratingsto.addItem(" ");
+		for(int i = 1; i < 11; i++){
+			ratingstoList.add(i);
+			ratingsto.addItem(i);
+		}
+		innerPan4.add(ratingstolb);
+		innerPan4.add(ratingsto);
+
+		// Genre
+		JPanel innerPan5 = new JPanel();
+		innerPan5.setLayout(new BoxLayout(innerPan5, BoxLayout.X_AXIS));	
+		JLabel genrelb = new JLabel("Genre  ");
+		JComboBox genre = new JComboBox();
+		String[] genres = new String[] {"Drama", "Comedy", "Thriller", "Horror"};
+		JComboBox<String> genresList = new JComboBox<>(genres);
+		innerPan5.add(genrelb);
+		innerPan5.add(genresList);
+
+		//search button
+		JPanel innerPan6 = new JPanel();
+		innerPan6.setLayout(new BorderLayout());
 		btnSearch = new JButton("Search");
 		ActionHandler handler = new ActionHandler();
 
-        btnSearch.addActionListener(handler);
-        jtxtFieldName.addActionListener(handler);
-        jtxtFieldYear.addActionListener(handler);
-		innerPan3.add(btnSearch,BorderLayout.CENTER);
-		
+		//listeners
+		btnSearch.addActionListener(handler);
+		jtxtFieldName.addActionListener(handler);
+		jtxtFieldYearFrom.addActionListener(handler);
+		innerPan6.add(btnSearch,BorderLayout.CENTER);
 		
 		//Table
 		/*JTable movieTalbe = new JTable();
@@ -128,33 +182,36 @@ public class yamDB {
 		dtm.setColumnIdentifiers(columNames);
 		movieTalbe.setModel(dtm);
 		movieTalbe.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
-		
 		dtm.addRow(new Object[] {"The Secret Life of Pets","2016","7.5"});
 		dtm.addRow(new Object[] {"The BFG","2016","8.5"});
 		dtm.addRow(new Object[] {"Ghostbusters","2016","5.5"});
 		dtm.addRow(new Object[] {"Central Intelligence","2016","8.0"});*/
+		
 		model = new DefaultListModel<>();
-         JList list = new JList(model);
-        
-
-
+		JList list = new JList(model);
 		panel.add(lbIcon);
+		panel.add(Box.createVerticalStrut(30));
+		panel.add(innerPan1);
 		panel.add(Box.createVerticalStrut(30));
 		panel.add(innerPan);
 		panel.add(Box.createVerticalStrut(15));
-		panel.add(innerPan2);
-		panel.add(Box.createVerticalStrut(15));
 		panel.add(innerPan3);
 		panel.add(Box.createVerticalStrut(15));
-		 panel.add(new JScrollPane(list));
+		panel.add(innerPan2);
+		panel.add(Box.createVerticalStrut(15));
+		panel.add(innerPan4);
+		panel.add(Box.createVerticalStrut(15));
+		panel.add(innerPan5);
+		panel.add(Box.createVerticalStrut(15));
+		panel.add(innerPan6);
+		panel.add(Box.createVerticalStrut(15));
+		panel.add(new JScrollPane(list));
 		//panel.add(new JScrollPane(movieTalbe));
 		frame.add(panel);
 		frame.pack();
 		frame.setVisible(true);
-
-
 	}
-	
+
 	private ImageIcon createImageIcon(String path, String description) {
 		java.net.URL imgURL = getClass().getResource(path);
 		if (imgURL != null) {
@@ -164,78 +221,70 @@ public class yamDB {
 			return null;
 		}
 	}
-	 public class ActionHandler implements ActionListener {
-     	
-
-         @Override
-         public void actionPerformed(ActionEvent e) {
-         	model.removeAllElements();
-         	try {
-
-     			//1.Get connection to database
-//     			Connection myCon = DriverManager.getConnection("jdbc:mysql://LocalHost/world", "root", "Lita1386!"); // replace world with the name of your database, put your password in ""
-         		Connection myCon = DriverManager.getConnection("jdbc:mysql://yamdb.ddns.net/yamdb?useSSL=false", "devs", "P@ssw0rd"); // replace world with the name of your database, put your password in ""
-         		
-         		//2. Create a statement
-     			Statement myStmt = myCon.createStatement();
-     			//3 Execute SQl query
-     			String name = jtxtFieldName.getText();
-     			String date = jtxtFieldYear.getText();
-     			String rank = null;
-     			String votes = null;
-     			/*String query = "select * from test WHERE name = ?";
+	
+	public class ActionHandler implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			model.removeAllElements();
+			try {
+				//1.Get connection to database
+				//Connection myCon = DriverManager.getConnection("jdbc:mysql://LocalHost/world", "root", "password"); // replace world with the name of your database, put your password in ""
+				Connection myCon = DriverManager.getConnection("jdbc:mysql://yamdb.ddns.net/yamdb?useSSL=false", "devs", "P@ssw0rd"); // replace world with the name of your database, put your password in ""
+				//2. Create a statement
+				Statement myStmt = myCon.createStatement();
+				//3 Execute SQl query
+				String name = jtxtFieldName.getText();
+				String date = jtxtFieldYearFrom.getText();
+				String rank = null;
+				String votes = null;
+				/*String query = "select * from test WHERE name = ?";
      			PreparedStatement pst = (PreparedStatement) myCon.prepareStatement(query);
      			pst.setString(1, "%" + searchText + "%");
      			ResultSet rs = pst.executeQuery();*/
-     		String searchStatement = "select * from Ratings";
-         		//also search date if we were sent one
-     		String outputString = "";
-     		boolean nothingToSearch = false;
-     		if(date.length() == 4 && (name.length() > 0 && name != null)) {
-     			searchStatement += " WHERE Title = '" + name + "' AND Year = '" + date + "'";
-     		}
-     		else if(date.length() == 4) {
-         		searchStatement += " WHERE Year = '" + date + "'";
-         	}
-     		else if(name.length() > 0 && name != null) {
-         		searchStatement += " WHERE Title = '" + name + "'";
-         	}
-     		else {
-     			nothingToSearch = true;
-     			outputString = "Sorry, I didn't recieve valid information to search. Check your date or title.";
-     		}
-     		ResultSet rs = myStmt.executeQuery(searchStatement);
-     			
-     			//ResultSet rs = myStmt.executeQuery("select * from test");
-     			
-     			
-     			//4 Process the result set
-     			String previousOutput = "";
-     			 while(rs.next()) {
-     				name = rs.getString("Title");
-     	     		System.out.println(name);
-     				date = rs.getString("Year").substring(0, 4);
-     				rank = rs.getString("Rank");
-     				votes = rs.getString("Votes");
-     				if(!nothingToSearch) {
-     					outputString = "Movie: " + name + "    Release Date: " + date + "    Rating: " + rank + "    Votes: " + votes;
-     				}
-     				if(!outputString.equals(previousOutput)) {
-     					model.addElement(outputString);
-     					previousOutput = outputString;
-     				}
-     				
-     	         }
-     	        /*if (searchText.equals(query)) {
+				String searchStatement = "select * from Ratings";
+				//also search date if we were sent one
+				String outputString = "";
+				boolean nothingToSearch = false;
+				if(date.length() == 4 && (name.length() > 0 && name != null)) {
+					searchStatement += " WHERE Title = '" + name + "' AND Year = '" + date + "'";
+				}
+				else if(date.length() == 4) {
+					searchStatement += " WHERE Year = '" + date + "'";
+				}
+				else if(name.length() > 0 && name != null) {
+					searchStatement += " WHERE Title = '" + name + "'";
+				}
+				else {
+					nothingToSearch = true;
+					outputString = "Sorry, I didn't recieve valid information to search. Check your date or title.";
+				}
+				ResultSet rs = myStmt.executeQuery(searchStatement);	
+				//4 Process the result set
+				String previousOutput = "";
+				int n = 1;
+				while(rs.next()) {
+					name = rs.getString("Title");
+					System.out.println(name);
+					date = rs.getString("Year").substring(0, 4);
+					rank = rs.getString("Rank");
+					votes = rs.getString("Votes");
+					if(!nothingToSearch) {
+						outputString = n + ". Movie Title: " + name + "    Release Date: " + date + "    Rating: " + rank + "    Votes: " + votes;
+						n++;
+					}
+					if(!outputString.equals(previousOutput)) {
+						model.addElement(outputString);
+						previousOutput = outputString;
+					}
+				}
+				/*if (searchText.equals(query)) {
      	            System.out.println("Searching names.."  + query);
      	            }*/
-     			
-     		} 
-     		catch (SQLException g) {
-     			// TODO Auto-generated catch block
-     			g.printStackTrace();
-     		}
-         }
-     }
-	
+			} 
+			catch (SQLException g) {
+				// TODO Auto-generated catch block
+				g.printStackTrace();
+			}
+		}
+	}
 }
